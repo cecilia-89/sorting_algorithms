2404cd1bcc58dac302c352881408d0569cd515fb
cecilia-89 <catabong89@gmail.com>
2022-08-01 15:06:52 +0000

    updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,23 +1,33 @@
 #include "sort.h"
 
 /**
- * quick_sort - sorts an array of int
- * @array: array of int
- * @size: size of the array
- *
+ * swap - swaps two elements together
+ * @a: first num to swap
+ * @b: second num to swap
  */
 
 void swap(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
+/**
+ * partition - sorts an array of int
+ * @array: array of int
+ * @start: first array index
+ * @end: last array index
+ * @size: size of the array
+ * Return: the index of the pivot
+ */
+
 int partition(int *array, int start, int end, size_t size)
 {
 	int index, pivot, i;
+
 	index = start;
 	pivot = array[end];
 
@@ -36,19 +46,35 @@ int partition(int *array, int start, int end, size_t size)
 	if (index != i)
 		print_array(array, size);
 
-	return index;
+	return (index);
 }
+
+/**
+ * quick_sort_array - sorts an array of int
+ * @array: array of int
+ * @start: the start of the index
+ * @end: the end of the index
+ * @size: size of the array
+ */
 
 void quick_sort_array(int *array, int start, int end, size_t size)
 {
 	int pindex;
+
 	if (start < end)
 	{
 		pindex = partition(array, start, end, size);
+
 		quick_sort_array(array, start, pindex - 1, size);
 		quick_sort_array(array, pindex + 1, end, size);
 	}
 }
+
+/**
+ * quick_sort - driver code
+ * @array: array to sort
+ * @size: the size of the array
+ */
 
 void quick_sort(int *array, size_t size)
 {
